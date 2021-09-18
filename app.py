226f5c49ec2e9650a622ef44f3f3c0b1f059004f
yeohsiewning <90822375+yeohsiewning@users.noreply.github.com>
2021-09-18 22:57:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  if request.method == "POST":
+    hrs = request.form["hrs"]
+  
+  return render_template("index.html")
 
