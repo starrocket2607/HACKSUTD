786d3d0a7762028c1387ac463770518f01f53e6d
Ignite99 <75031614+Ignite99@users.noreply.github.com>
2021-09-18 23:02:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,10 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+##link to main page
+def link():
     return render_template("index.html")
 
-@app.route("/sub", methods = ['POST'])
-def submit():
-    if request.method == "POST":
-        name = request.form["username"]
-    return render_template("sub.html", n = name)
 
 if __name__ == "__main__":
     app.run(debug=True)
