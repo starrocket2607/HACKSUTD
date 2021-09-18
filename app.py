f16f18806a3fc7395e6cf9f6cf94c7268fc43b30
Ignite99 <75031614+Ignite99@users.noreply.github.com>
2021-09-18 22:58:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  if request.method == "POST":
-    hrs = request.form["hrs"]
-  
-  return render_template("index.html")
+    return render_template("index.html")
 
+@app.route("/sub", methods = ['POST'])
+def submit():
+    if request.method == "POST":
+        name = request.form["username"]
+    return render_template("sub.html", n = name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
